b7e5e52e3a791d25f92128290a7c51af7935dbf8
Mauro Taliente <mauro.taliente3@gmail.com>
2018-04-19 19:43:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var $aprendiendo = 'maquetado'
+$aprendiendo = 'programacion'
+
+//la posta si tengo mas de 2 casos
+switch ($aprendiendo) {
+  case "maquetado":
+    console.log('declaraciones y propiedades para generar contenido');
+    break;
+  case "programacion":
+    console.log('casos para resolver instrucciones logicas');
+    break;
+}
---
+++ js/main.js
@@ -0,0 +1,17 @@
+// jQuery(document).ready(function() {
+//   //Login
+//
+//   const formLogin= $( '#login' )
+//   const fomrData = new formData(formLogin)
+//
+//   formLogin.submit(function( event ) {
+//     event.preventDefault();
+//     fetch('api/sigin', {
+//       method: 'POST',
+//       body: formData
+//     })
+// });
+// // .then(res => res.json())
+// // .then(data => console.log(data))
+//
+// });
